04583ab9299c3b992675dc2e9cdffcd398583daf
elizabenEighteen <1909669885@qq.com>
2018-12-13 09:03:28 +0800

    third day

--- js/comon.js
+++ js/comon.js
@@ -2,17 +2,20 @@
 (function(){
 	var head = document.createElement('header');
 	var foot = document.createElement('footer');
+	var navDialog = document.createElement('div');
+	navDialog.id = 'navDialog';
+
 	head.innerHTML = `
 		<img src="../img/img_logo.png" alt="">
 		<ul class="nav">
 			<li><a href="../html/prodotti.html">PRODOTTI</a></li>
 			<li><a href="../html/cataloghi.html">CATALOGHI</a></li>
-			<li><a href="../html/reteVendita.html">RETE VENDITA</a></li>
-			<li><a href="../html/contract.html">CONTRACT</a></li>
+			<li><a href="../html/team.html">TEAM</a></li>
+			<li><a href="../html/designers.html">DESIGNERS</a></li>
 		</ul>
 		<div class="buttons">
 			<a class="navBtn wishlist" href="javascript:"></a>
-			<a class="navBtn login" href="javascript:"></a>
+			<a class="navBtn login" href="../html/login.html"></a>
 			<a class="navBtn search" href="javascript:"></a>
 			<a class="navBtn more" href="javascript:"></a>
 		</div>
@@ -60,8 +63,26 @@
 			</ul>
 		</div>
 		`
+		navDialog.innerHTML = `
+		<ul>
+			<li><a href="../html/prodotti.html">PRODOTTI</a></li>
+			<li><a href="../html/cataloghi.html">CATALOGHI</a></li>
+			<li><a href="../html/team.html">TEAM</a></li>
+			<li><a href="../html/designers.html">DISIGNERS</a></li>
+			<li><a href="../html/chiSiamo.html">CHI SIAMO</a></li>
+			<li><a href="../html/myLife.html">MY LIFE</a></li>
+			<li><a href="../html/news.html">NEWS</a></li>
+			<li><a href="../html/login.html">PRESS RELEASE</a></li>
+			<li><a href="../html/contatti.html">CONTATTI</a></li>
+			<li><a href="../html/newsletters.html">NEWSLETTERS</a></li>
+			<li><a href="../html/areaRiservata.html">AREA RISERVATA</a></li>
+		</ul>
+		<div class="close"></div>
+	`
 	document.body.appendChild(head);
 	document.body.appendChild(foot);
+	document.body.appendChild(navDialog);
+	
 })();
 /*click logo*/
 	
@@ -70,4 +91,17 @@
 	$logoBtn.on('click',function(){
 		window.location.href = '../index.html';
 	});
-})()
+})();
+/*navDialog*/
+(function(){
+	var $navBtn = $('header .more');
+	var $navDialog = $('#navDialog');
+	var $close = $navDialog.find('.close');
+	$navBtn.on('click',function(){
+
+		$navDialog.show();
+	});
+	$close.on('click',function(){
+		$navDialog.hide();
+	});
+})();
--- js/index.js
+++ js/index.js
@@ -4,4 +4,17 @@ $(function(){
 	$logoBtn.on('click',function(){
 		window.location.href = './index.html';
 	});
+	/*navDialog*/
+	(function(){
+		var $navBtn = $('header .more');
+		var $navDialog = $('#navDialog');
+		var $close = $navDialog.find('.close');
+		$navBtn.on('click',function(){
+
+			$navDialog.show();
+		});
+		$close.on('click',function(){
+			$navDialog.hide();
+		});
+	})();
 });
